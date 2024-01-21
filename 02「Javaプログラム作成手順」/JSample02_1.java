65d0951417497ba623249efafb7a34a4d26fa122
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2024-01-21 15:50:07 +0000

    [Add] 「Javaプログラム作成手順」の学習内容のファイルを追加

---
+++ 02「Javaプログラム作成手順」/JSample02_1.java
@@ -0,0 +1,5 @@
+class JSample02_1 {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
---
+++ 02「Javaプログラム作成手順」/JSample02_2.java
@@ -0,0 +1,6 @@
+// 文法エラーのため、コンパイル時にエラーが発生する
+class JSample02_2 {
+  public static void main(Sting[] args) {
+    System.auto.println("Hello");
+  }
+}
