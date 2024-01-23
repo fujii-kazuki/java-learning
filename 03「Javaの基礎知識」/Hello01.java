ace362820d22a7e4fe960e373e9edbe8440520cd
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2024-01-23 14:27:33 +0000

    [Add] 「Javaの基礎知識」の学習内容を更新

---
+++ 03「Javaの基礎知識」/Hello01.java
@@ -0,0 +1,6 @@
+class Hello01 {
+  // mainメソッドが定義されていない為、コンパイルは可能だが実行時にエラーが発生
+  public static void greeting (String[] args) {
+    System.out.println("Hello");
+  }
+}
---
+++ 03「Javaの基礎知識」/Hello02.java
@@ -0,0 +1,7 @@
+class Hello02 {
+  // mainメソッドにpublicやstaticが記述されておらず、外部から呼び出すことが出来ないため、
+  // 実行時にエラーが発生する
+  void main (String[] args) {
+    System.out.println("Hello");
+  }
+}
