2975f696a4c4ad36fdc5f56c7b1b31f7818b532d
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2024-02-01 22:02:46 +0900

    [Add] 「演算子」の学習内容のファイルを更新

--- 07「演算子」/JSample7_1.java
+++ 07「演算子」/JSample7_1.java
@@ -1,8 +1,22 @@
 class JSample7_1 {
   public static void main(String[] args) {
+    // int型同士で除算を行う際の注意点
     int num1 = 10;
     int num2 = 4;
 
-    System.out.println(num1 / (double)num2);  //2.5
+    System.out.println(num1 / num2);  //2（間違い）
+    // 演算子の左辺か右辺どちらかをキャスト演算子でdouble型に変換して計算
+    System.out.println(num1 / (double)num2);  //2.5（正しい）
+
+    // ビット演算子
+    int i1 = 85 & 15;
+    int i2 = 85 | 15;
+    int i3 = 85 ^ 15;
+    int i4 = ~85;
+
+    System.out.println("85 & 15 = " + i1);
+    System.out.println("85 | 15 = " + i2);
+    System.out.println("85 ^ 15 = " + i3);
+    System.out.println("~85 = " + i4);
   }
 }
