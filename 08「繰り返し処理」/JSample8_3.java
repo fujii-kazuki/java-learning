adb4e31665126d1191034e83f3e811032c62eaa7
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2024-02-12 22:02:59 +0900

    [Update] 「演算子」の学習内容を更新

---
+++ 08「繰り返し処理」/JSample8_3.java
@@ -0,0 +1,8 @@
+class JSample8_3 {
+  public static void main(String[] args) {
+    // 複数の初期化式と変化式を使用したfor文
+    for (int i = 1, j = 5; i <= 5; i++, j--) {
+      System.out.println("i = " + i + ", j = " + j);
+    }
+  }
+}
